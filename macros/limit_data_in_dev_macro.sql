b3e16f39646e33941bd6ffed1cc7eeecf2d1a9c7
Daniel-hub-es <Valbuena1v2b@gmail.com>
2025-06-01 13:15:51 +0100

    adjust macro for stg_orders model

--- macros/limit_data_in_dev_macro.sql
+++ macros/limit_data_in_dev_macro.sql
@@ -1,13 +1,9 @@
 {%- macro limit_data_in_dev(column, days) -%}
 
     {%- if target.name == 'dev' -%}
-
-        where {{ column }} >= dateadd('day', -{{ days }}, current_timestamp)
-
+        where {{ column }} >= current_timestamp - interval '{{ days }} days'
     {%- else -%}
-
         -- Macro skipped in env: {{ target.name }}
-
     {%- endif -%}
 
 {%- endmacro -%}
