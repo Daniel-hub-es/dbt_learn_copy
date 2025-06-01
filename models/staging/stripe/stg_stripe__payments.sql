8c06493c560569bdf378e47360bf550eb1222112
Daniel-hub-es <Valbuena1v2b@gmail.com>
2025-06-01 13:16:17 +0100

    add stg pyaments model

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -7,13 +7,13 @@ with
 
     payments as(
         select 
-            id as payment_id,
-            orderid as order_id,
-            paymentmethod as payment_method,
-            status as payment_status,
-            {{ cents_to_dollars('amount') }} as amount,
-            round(amount/10.0, 2) as payment_amount,
-            created as created_at
+            "ID" as payment_id,
+            "ORDERID" as order_id,
+            "PAYMENTMETHOD" as payment_method,
+            "STATUS" as payment_status,
+            {{ cents_to_dollars('"AMOUNT"') }} as amount,
+            round("AMOUNT"/10.0, 2) as payment_amount,
+            "CREATED" as created_at
         from source
     )
 
