126d0c4bc6442a6b836cfbd3a5b9b332fab4689d
Daniel-hub-es <Valbuena1v2b@gmail.com>
2025-06-03 14:57:02 +0100

    Add battery of tests

---
+++ tests/assert_stg_payments_ammount_is_positive.sql
@@ -0,0 +1,15 @@
+-- deteting orders where the total amount isn't positive 
+
+with
+
+    payments as (
+        select *
+        from {{ ref('stg_stripe__payments') }}
+    )
+
+select
+    order_id,
+    sum(amount) as total_amount
+from payments
+group by order_id
+having total_amount < 0
---
+++ tests/generic/assert_column_is_greater_than_five.sql
@@ -0,0 +1,11 @@
+{% test assert_column_is_greater_than_five(model, column_name) %}
+
+{{ config(severity = 'warn') }}
+
+select 
+    {{ column_name }}
+from {{ model }}
+group by {{ column_name }}
+having {{ column_name }} <= 5
+
+{% endtest %}
